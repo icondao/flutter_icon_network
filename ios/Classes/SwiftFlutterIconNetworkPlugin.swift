f4d3216e955ab27076edb8a4c0b627fd647681a0
Leo <quanngovan98@gmail.com>
2021-03-25 10:04:17 +0700

    add template code

--- ios/Classes/SwiftFlutterIconNetworkPlugin.swift
+++ ios/Classes/SwiftFlutterIconNetworkPlugin.swift
@@ -11,10 +11,24 @@ public class SwiftFlutterIconNetworkPlugin: NSObject, FlutterPlugin {
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
     case "createWallet":
+        var wallet: Dictionary<String, String> = [:]
+        wallet["private_key"] = "test pr key"
+        wallet["address"] = "address"
+        result("{\"private_key\":\"\(wallet["private_key"]!)\",\"address\":\"\(wallet["address"]!)\"}")
         break
     case "sendIcx":
+        let argumentsMap = (call.arguments as! Dictionary<String, String>)
+        let from = argumentsMap["from"]
+        let to = argumentsMap["to"]
+        let value = argumentsMap["value"]
+        let txHash = "testHash"
+        result("{\"txHash\":\"\(txHash)\",\"status\":0}")
         break
     case "getBalance":
+        let argumentsMap = (call.arguments as! Dictionary<String, String>)
+        let privateKey = argumentsMap["private_key"]
+        let balance = "100000000000000"
+        result("{\"balance\":\"\(balance)\"}")
         break
     default:
         print("unhandled method call \(call.method)")
